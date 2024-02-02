c2c2227c07dff3e17d4554b03a9f7b515a71a49f
alumno <anwarneggadi05@gmail.com>
2024-02-02 11:38:11 +0100

    DUdi

--- prueba.py
+++ prueba.py
@@ -1,15 +1,18 @@
 from itertools import combinations
-
+def baraja():
 # Definir los palos y los valores de las cartas
-palos = ['Corazones', 'Diamantes', 'Tréboles', 'Picas']
-valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    palos = [chr(3), chr(4), chr(5), chr(6)]
+    valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 # Crear una baraja completa
-baraja = [(valor, palo) for valor in valores for palo in palos]
+    baraja = [(valor, palo) for valor in valores for palo in palos]
 
 # Generar todas las combinaciones posibles de 5 cartas (mano de poker)
-combinaciones_poker = list(combinations(baraja, 5))
+    for f in range(5):
+        combinaciones_poker = list(combinations(baraja[f], 1))
 
 # Imprimir las primeras 5 combinaciones como ejemplo
-for i in range(5):
-    print(f"Combinación {i + 1}: {combinaciones_poker[i]}")
+    for i in range(5):
+        print(f"Combinación {i + 1}: {combinaciones_poker[i]}")
+
+baraja()
